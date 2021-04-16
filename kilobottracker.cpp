b5c94cf946eeeea2a571943244ba8b286ac95c9a
luigifeola <feola@diag.uniroma1.it>
2021-04-16 14:03:52 +0200

    increased e well positioned IDs, unchecked as default green detection and runtime identification

--- kilobottracker.cpp
+++ kilobottracker.cpp
@@ -638,7 +638,7 @@ void KilobotTracker::SETUPfindKilobots()
         //circle( result, center, 3, Scalar(0,255,0), -1, 8, 0 );
         // draw the circle outline
         circle( display, center, radius, Scalar(0,0,255), 3, 8, 0 );
-        putText(display, this->showIDs?to_string(i):"", center, FONT_HERSHEY_PLAIN, 2.7, Scalar(0,0,255), 3);
+        putText(display, this->showIDs?to_string(i):"", center-Point(0,33), FONT_HERSHEY_PLAIN, 3.5, Scalar(0,0,255), 3);
     }
 
     cv::resize(display,display,Size(this->smallImageSize.x()*2, this->smallImageSize.y()*2));
@@ -1276,7 +1276,7 @@ void KilobotTracker::trackKilobots()
                 //qDebug() << "Single vel is" << this->kilos[i]->getVelocity() << "AVG vel is" << this->kilos[i]->velocityBuffer.getAvgOrientation();
 
                 if (this->showIDs) {
-                    cv::putText(display, QString::number(this->kilos[i]->getID()).toStdString(), Point(kilos[i]->getPosition().x(),kilos[i]->getPosition().y()), FONT_HERSHEY_PLAIN, 2.7, rgbColor, 3);
+                    cv::putText(display, QString::number(this->kilos[i]->getID()).toStdString(), Point(kilos[i]->getPosition().x(),kilos[i]->getPosition().y()-33), FONT_HERSHEY_PLAIN, 3.5, rgbColor, 3);
                 }
             }
             this->drawOverlay(display);
